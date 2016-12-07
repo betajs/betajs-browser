e1f9c1e205240d41f8ac9f3e21111a0783a97636
Rashad Aliyev <aliyev.resad@gmail.com>
2016-12-07 16:51:34 +0400

    Sample configuration file was modified

--- browserstack/configurations/constants-sample.js
+++ browserstack/configurations/constants-sample.js
@@ -1,17 +1,22 @@
 module.exports = {
   browserstack : {
-    "version": "",
+    "version": "v1",
     "user": "",
     "key": "",
     "bas_url": "http://localhost:6001",
     "related_url": "/static/space.html",
     "local_test": true,
+    "report_source_folder": "./browserstack/reports",
     "host" : 'http://hub-cloud.browserstack.com',
     "port": 80,
     "path": '/wd/hub',
     "acceptSslCerts" : true,
     "debug": true,
-    "videoRecord": false,
+    "videoRecord": true,
+    "upload" : {
+      "bas_url": "http://192.168.0.101:5000",
+      "related_url": "/static/tests/files/index.html"
+    },
     "ie" : {
       "noFlash" : true,  // Disable Flash in IE
       "enablePopups" : true
